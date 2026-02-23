42352fe0cdf050602ce676e0b14822fbc67f2b1e
Pottabathini Vivekananda <vivekananda.99666@gmail.com>
2026-02-23 19:39:08 +0530

    add base url

--- src/hooks/useScheduler.js
+++ src/hooks/useScheduler.js
@@ -29,7 +29,7 @@ export const ALGORITHM_DESCRIPTIONS = {
   7: 'Multi-Level Feedback Queue',
 };
 
-const API = 'https://try-flask-ftit.onrender.com/api/v2';
+const API = '/api/v2';
 
 const emptyMetrics = {
   avgWaitTime: 0, avgTurnaroundTime: 0, avgResponseTime: 0,
