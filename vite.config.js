3050e6ab6e2f9def6aa112746437ecb58a24c923
Pottabathini Vivekananda <vivekananda.99666@gmail.com>
2026-02-23 19:40:52 +0530

    add base url

--- vite.config.js
+++ vite.config.js
@@ -7,11 +7,11 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
-    proxy: {
-      '/api': {
-        target: 'https://try-flask-ftit.onrender.com',
-        changeOrigin: true
-      }
-    }
+    // proxy: {
+    //   '/api': {
+    //     target: 'https://try-flask-ftit.onrender.com',
+    //     changeOrigin: true
+    //   }
+    // }
   }
 })
